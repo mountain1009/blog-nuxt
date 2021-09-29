5548c3febd023c3b8024e3ebafa2fa9eb38883fb
mountain1009 <mountain.09hand@gmail.com>
2021-09-29 22:54:36 +0900

    top

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,32 @@
 <template>
   <article>
-    <div class="p-eye-catch"></div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-2">
+      <div class="text-center" v-for="doc in docs" :key="doc.id">
+        <lazy-component>
+          <nuxt-link :to="doc.path">
+            <article-image :name="doc.image" alt="サンプル画像"/>
+            {{doc.title}}
+          </nuxt-link>
+        </lazy-component>
+      </div>
+    </div>
   </article>
 </template>
 
-<script setup lang="ts">
-import {  useContext, ref, useFetch } from '@nuxtjs/composition-api'
+<script lang="ts">
+import {  defineComponent,useContext, ref, useFetch } from '@nuxtjs/composition-api'
 
-const {$content} = useContext()
-const docs = ref<unknown>([])
+export default defineComponent({
+  head:{},
+  setup() {
+    const {$content} = useContext()
+    const docs = ref<unknown>([])
 
-useFetch(async ()=>{
-  docs.value = await $content("articles").fetch()
+    useFetch(async ()=>{
+      docs.value = await $content("articles").fetch()
+    })
+
+    return { docs }
+  },
 })
-
 </script>
-
-<style lang="css">
-.p-eye-catch{
-  background: blanchedalmond;
-}
-</style>
